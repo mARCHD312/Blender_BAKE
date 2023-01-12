7a6441b0751d0594395d23ad5bbf6515a36ea091
Bojan Mikulic <bojan.mikulic@doob3dlabs.com>
2023-01-12 14:17:51 +0100

    Working with O

--- bake.py
+++ bake.py
@@ -108,6 +108,7 @@ def check_mtls_and_meshes():
             all_mtls.append(obj.material_slots[0].name)
     
     mtls_with_more_instances = [item for item, count in collections.Counter(all_mtls).items() if count > 1]
+    print(mtls_with_more_instances)
     # mtl_with_one_instance = [mtl for mtl in all_mtls if mtl not in mtls_with_more_instances]
 
     return mtls_with_more_instances
@@ -147,31 +148,55 @@ def delete_unused_uv_maps():
                 except Exception as e:
                     print("Exception with deleteing usused uw maps: ")
                     print(e)            
-
+    
+def delete_uv_maps_of_objects_that_starts_with_O():
+    for obj in bpy.data.objects:
+            if  obj.type == "MESH" and obj.name.startswith("O"):
+                try:
+                    used_layer = obj.material_slots[0].material.node_tree.nodes['UV Map'].uv_map
+                    # for uv_layer in obj.data.uv_layers:
+                    #     if uv_layer.name != used_layer:
+                    a = obj.data.uv_layers.get(used_layer.name)
+                    obj.data.uv_layers.remove(a)
+                except Exception as e:
+                    print("Exception with deleteing usused uw maps: ")
+                    print(e)
+                    
+                    
+    for obj in bpy.data.objects:
+        if obj.type == "MESH" and obj.name.startswith("O"):
+            for mat in obj.data.materials:
+                for node in mat.node_tree.nodes:                
+                    if node.name == "UV Map": # check this when have time
+                            mat.node_tree.nodes.remove(node)
+                    #     pass
+                    # else:
+                    #     counter = 0
+                    #     for outp in node.outputs:                        
+                    #         if len(outp.links) >0:
+                    #             counter = counter + 1                    
+                    #     if counter == 0:
+    
 
 
 def prepare_bake_save():
     cwd = os.getcwd()
     deselect_all()
     mtls_with_more_instances =  check_mtls_and_meshes()
-       
 
-    # specific_objects = [bpy.data.objects['Box001']]
-    #     bpy.data.objects['booth2'],
-    #     bpy.data.objects['door3'],
-    #     bpy.data.objects['floor_pos_01'],
-    #     bpy.data.objects['floor_pos_02'],
-    #     bpy.data.objects['floor_pos_03'],
-    #     bpy.data.objects['ict_board'],
-    #     bpy.data.objects['ict_board1'],
-    #     bpy.data.objects['Portal'],
-    #     bpy.data.objects['Vault_gymnastics_01']]
-    # for obj in specific_objects:  # for specific objects 
+    # for every objecty in coollection
     for obj in bpy.data.collections['for_bake'].objects:
-    #for obj in bpy.data.objects:  # for all objects
+    # # for obj in bpy.data.objects:  
+    
+    # # if object have less than two material slots and is mesh tye
         if len(obj.material_slots)<2:
+            # pass
             if  obj.type == "MESH":
+                print("ONE MTL")
+                print(obj.name)
+    #             # if materisl name in materiales with more instances
                 if obj.material_slots[0].name in mtls_with_more_instances:                          
+                    pass
                     change_to_mode("OBJECT")
                     deselect_all()
                     obj.select_set(True)
@@ -211,8 +236,8 @@ def prepare_bake_save():
                         bpy.ops.object.bake(type='COMBINED')         
                     except:
                         pass                     
-
-
+                    
+                    
                      
                     bpy.data.images[f"{baked_photo_name}.jpeg"].save_render(baked_photo_path)
                     mat = bpy.data.materials.new(name=name_mtl)
@@ -246,7 +271,12 @@ def prepare_bake_save():
                         if obj.name.startswith("O"):
                             pass
                         else:
-                            unwrap(obj,"UVMap")      
+                            uv_names_list = [x.name for x in obj.data.uv_layers]
+                            if "UVMap" in uv_names_list:
+                                unwrap(obj,'UVMap.001')
+                            else:
+                                unwrap(obj,'UVMap')
+                                
                         bake_connect_output(obj,image_texture_node,baked_photo_name,baked_photo_path)
                         
                     except Exception as e:
@@ -254,6 +284,8 @@ def prepare_bake_save():
                         
 
         else:
+            print("MORE MTLS:")
+            print(obj.name)
             change_to_mode("OBJECT")
             deselect_all()
             obj.select_set(True)
@@ -261,11 +293,15 @@ def prepare_bake_save():
             image_texture_node, baked_photo_name, baked_photo_path =  create_photo(cwd,obj)
             name_mtl = f"{obj.name}_mtl"
             uv_names_list = [x.name for x in obj.data.uv_layers]
+            
+            if obj.name.startswith("O"):
+                pass
 
-            if "UVMap" in uv_names_list:
-                unwrap(obj,'UVMap.001')
             else:
-                unwrap(obj,'UVMap')
+                if "UVMap" in uv_names_list:
+                    unwrap(obj,'UVMap.001')
+                else:
+                    unwrap(obj,'UVMap')
 
             for count, mtl in enumerate(obj.material_slots):
                 obj.active_material_index = count
@@ -345,29 +381,31 @@ def createing_atlas():
 
 def rename_uv_layers():
     for obj in bpy.data.objects:
-        try:
+        # try:
+        if obj.type == "MESH":
             obj.data.uv_layers[0].name = 'map1'
         # except e as Exception:
             # print(e)
-        except:
-            pass
+        # except:
+        #     pass
 
 def main():
     start_time = time.time()
-    bpy.context.scene.cycles.samples = 2048
+    bpy.context.scene.cycles.samples = 10
     bpy.context.scene.cycles.adaptive_threshold = 0.05
     rename_uv_layers()
-    # raise
-    print("BAke start")
+    # # raise
+    # print("BAke start")
     prepare_bake_save()
 
     delete_unused_nodes()
-    # delete_unused_uv_maps()
+    delete_uv_maps_of_objects_that_starts_with_O()
+    # # delete_unused_uv_maps()
     # raise
     # createing_atlas()    
     end_time = time.time()
     save_to_blend_file(SAVE_FILE)
-    bpy.ops.export_scene.gltf(filepath=SAVE_FILE[:-5] + 'gltf')
+    # bpy.ops.export_scene.gltf(filepath=SAVE_FILE[:-5] + 'gltf')
     print("finished in: ")
     print(end_time-start_time)
 
